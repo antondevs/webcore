1b2ffce9ac9f1b6451d0a09d1d03a21305d8b0c8
Anton Nadtoka <anton.nadtoka@yandex.ru>
2019-06-19 09:27:04 +0300

    Add default user

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -36,16 +36,31 @@
     <div class="card ">
         <div class="card-header text-center"><a href="../index.html"><img class="logo-img" src="../assets/images/logo.png" alt="logo"></a><span class="splash-description">Please enter your user information.</span></div>
         <div class="card-body">
-            <form>
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
                 <div class="form-group">
-                    <input class="form-control form-control-lg" id="username" type="text" placeholder="Username" autocomplete="off">
+                    <input class="form-control form-control-lg" id="email" name="email" type="text"
+                           placeholder="Email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                    @error('email')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
                 </div>
                 <div class="form-group">
-                    <input class="form-control form-control-lg" id="password" type="password" placeholder="Password">
+                    <input class="form-control form-control-lg" id="password" type="password"
+                           placeholder="Password" name="password" required autocomplete="current-password">
+                    @error('password')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
                 </div>
                 <div class="form-group">
                     <label class="custom-control custom-checkbox">
-                        <input class="custom-control-input" type="checkbox"><span class="custom-control-label">Remember Me</span>
+                        <input class="custom-control-input" type="checkbox"
+                               name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                        <span class="custom-control-label">Remember Me</span>
                     </label>
                 </div>
                 <button type="submit" class="btn btn-primary btn-lg btn-block">Sign in</button>
